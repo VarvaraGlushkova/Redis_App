bf07c8578e082fd9b24d458cf56c5128260c504e
VarvaraGlushkova <varvara.glushkova@icloud.com>
2025-05-23 11:34:07 +0300

    not much changed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
+  # validates :name, presence: true
 
   mount_uploader :image, TaskImageUploader
   # validates :bio, length: { maximum: 300 }
